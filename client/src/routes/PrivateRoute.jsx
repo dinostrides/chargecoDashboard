6c464b0a58b733af8bc573625dd3dd47f1e287a2
dinostrides <dino03@smrt.com.sg>
2024-09-13 09:52:44 +0800

    refresh token in private route
    issue: with expired token
    - you cannot refresh the page, token will not renew, will be logged out (Fixed)
    - you cannot manually edit the url to go to another endpoint, will be logged out (fixed)

--- client/src/routes/PrivateRoute.jsx
+++ client/src/routes/PrivateRoute.jsx
@@ -22,12 +22,37 @@ export const PrivateRoute = ({ children }) => {
 
         if (response.data.detail === "Token is valid") {
           setIsValid(true);
+        }
+
+      } catch (error) {
+        console.log('boost', error.response.data.detail);
+        
+        if (error.response.data.detail === "Invalid or expired token") {
+          try {
+            // Try to renew token
+            const refresh = localStorage.getItem('refreshToken');
+            if (refresh) {
+              const refreshResponse = await axios.post('http://localhost:8000/api/token/refresh/', {
+                refresh: refresh
+              });
+              
+              const newAccessToken = refreshResponse.data.access;
+              localStorage.setItem('accessToken', newAccessToken);
+              
+              setIsValid(true);
+              
+              // Update the jwtToken in state only after setting token in storage
+              setTimeout(() => {
+                setJwtToken(newAccessToken);  // Delay token update to prevent immediate rerun
+              }, 0);
+            }
+          } catch (error) {
+            console.log(error);
+            setIsValid(false); // Token refresh failed, log out or handle appropriately
+          }
         } else {
           setIsValid(false);
         }
-      } catch (error) {
-        console.log(error.message)
-        setIsValid(false);
       }
     };
 
@@ -42,3 +67,6 @@ export const PrivateRoute = ({ children }) => {
 };
 
 export default PrivateRoute;
+
+
+
